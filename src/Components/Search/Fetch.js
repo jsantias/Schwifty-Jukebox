a80bdf0386cd64e1cb22b5de40e994004f5d8dec
seanocca <32288543+seanocca@users.noreply.github.com>
2019-08-24 16:12:26 +1000

    Changed length of results returned and only video

--- src/Components/Search/Fetch.js
+++ src/Components/Search/Fetch.js
@@ -7,6 +7,8 @@ function Fetch(param, callback) {
   var params = {
     part: "snippet",
     q: param,
+    type: "video",
+    maxResults: 20,
   };
   client.search(params, function(err, data) {
     callback(data);
